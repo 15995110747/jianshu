ac3400125089901d3523605e67fc07d3417dc8f2
andrew <1060406280@qq.com>
2018-04-18 14:50:48 +0800

    第一个页面的创建

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,14 +5,12 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import bootstrapVue from 'bootstrap-vue'
+import Vue from "vue";
+import bootstrapVue from "bootstrap-vue";
 Vue.use(bootstrapVue);
-import 'bootstrap/dist/css/bootstrp.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-export default {
-  
-}
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+export default {};
 </script>
 
 <style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>HELLO</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+      return{
+          name:'首页'
+      }
+  }
+};
+</script>
+<style>
+
+</style>
+
