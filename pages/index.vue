a40aaf6751b7e0d74c2b969c14c89a8a0334bbe2
andrew <1060406280@qq.com>
2018-04-23 18:36:21 +0800

    首页完成

--- pages/index.vue
+++ pages/index.vue
@@ -121,6 +121,86 @@
                                   </div>
                               </div>
                           </li>
+                          <li class="hava-img">
+                              <nuxt-link to='/p/123' class="wrap-img">
+                                <img src="../assets/image/n1.jpg" alt="">
+                              </nuxt-link>
+                              <div class="content">
+                                  <div class="author">
+                                      <nuxt-link to='/p/123' class="avatar">
+                                        <img src="../assets/image/default-avator.jpg" alt="">
+                                      </nuxt-link>
+                                      <div class="info">
+                                          <nuxt-link  to='/u/123' class="nickname">
+                                            测试用户
+                                          </nuxt-link>
+                                          <span class="time">
+                                              04.12 10:18
+                                          </span>
+                                      </div>
+                                  </div>
+                                  <nuxt-link class="title" to='/p/123'>
+                                      佛系程序员的年薪五万指南
+                                  </nuxt-link>
+                                  <div class="abstruct">
+                                      文▏焱公子 假如生而不同，你将怎么面对这个世界？世界，又会怎样看你？ 今天我们要评析的这部电影，与同档期上映的《神秘巨星》有点儿像。一者，都是甜而不腻的鸡汤，二者，都是正向积... 
+                                  </div>
+                                  <div class="meta">
+                                      <nuxt-link class="collection-tag" to='/u/123'>
+                                        社会与法
+                                      </nuxt-link>
+                                      <nuxt-link  to='/p/123'>
+                                        <i class="fa fa-eye"></i>
+                                        1500
+                                      </nuxt-link>
+                                      <nuxt-link  to='/p/123'>
+                                        <i class="fa fa-comment"></i>
+                                        100
+                                      </nuxt-link>
+                                      <span>
+                                        <i class="fa fa-heart"></i>
+                                        20
+                                      </span>
+                                  </div>
+                              </div>
+                          </li>
+                          <li>
+                              <div class="content">
+                                  <div class="author">
+                                      <nuxt-link to='/p/123' class="avatar">
+                                        <img src="../assets/image/default-avator.jpg" alt="">
+                                      </nuxt-link>
+                                      <div class="info">
+                                          <nuxt-link  to='/u/123' class="nickname">
+                                            测试用户
+                                          </nuxt-link>
+                                          <span class="time">
+                                              04.12 10:18
+                                          </span>
+                                      </div>
+                                  </div>
+                                  <div class="title">
+                                      佛系程序员的年薪五万指南
+                                  </div>
+                                  <div class="abstruct">
+                                      文▏焱公子 假如生而不同，你将怎么面对这个世界？世界，又会怎样看你？ 今天我们要评析的这部电影，与同档期上映的《神秘巨星》有点儿像。一者，都是甜而不腻的鸡汤，二者，都是正向积... 
+                                  </div>
+                                  <div class="meta">
+                                      <nuxt-link  to='/p/123'>
+                                        <i class="fa fa-eye"></i>
+                                        1500
+                                      </nuxt-link>
+                                      <nuxt-link  to='/p/123'>
+                                        <i class="fa fa-comment"></i>
+                                        100
+                                      </nuxt-link>
+                                      <span>
+                                        <i class="fa fa-heart"></i>
+                                        20
+                                      </span>
+                                  </div>
+                              </div>
+                          </li>
                       </ul>
                       <a href="#" class="load-more">阅读更多</a>
                   </div>
